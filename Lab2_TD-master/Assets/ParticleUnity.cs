4ad3f15988309767a4afa1711cf4e9ba8977bb5b
TheChaoticLion <39880884+TheChaoticLion@users.noreply.github.com>
2020-03-23 08:20:55 -0400

    Lab 2
    Putting files ino github

---
+++ Lab2_TD-master/Assets/ParticleUnity.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleUnity : MonoBehaviour
+{
+    public Vector3 initialVel;
+    public Vector3 newPos;
+    public Rigidbody rb;
+    Vector3 updateForce;
+    public Vector3 TargetOffset;
+    public Transform MyParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the initial position of a spawned particle
+        //transform.position = Vector3.zero;
+        //set initial velocity of the spawned particle
+        initialVel = 0.0f * Vector3.one;
+        rb = GetComponent<Rigidbody>();
+        //rb.velocity = initialVel;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        //create a force field
+        newPos = transform.position;//get the particle postion
+        updateForce = Vector3.one;
+        Vector3 desiredPosition = MyParent.position + (MyParent.forward * TargetOffset.z)+(MyParent.up * TargetOffset.y)+(MyParent.right * TargetOffset.x);
+        //Vector3 desireddirection = desiredPosition - transform.position;
+        //rb.AddForce(desireddirection.normalized * 5f * desireddirection.magnitude);
+        Vector3 nextpos = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 3f);
+        rb.MovePosition(nextpos);
+        //push the particle witht the force field
+        //rb.AddForce(updateForce);
+
+    }
+}
---
+++ Lab2_TD-master/Assets/Spawner.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float SpawnRadius = 30f;
+
+    //This is a Singleton of the particle Spawner. there is only one instance 
+    // of this Spawner, so we can store it in a static variable named s.
+    static public Spawner S;
+    static public List<ParticleUnity> ParticleUnitys;
+
+    // These fields allow you to adjust the spawning behavior of the spheres
+    [Header("Set in Inspector: Spawning")]
+    public GameObject particleUnityPrefab;
+    public Transform particleAnchor;
+    public int numParticles = 100;
+    public float spawnDelay = 0.1f;
+    Vector3[] _SpawnPoints;
+    
+
+    //add variables here that spawned particles will inherit    
+
+    void Awake()
+    {
+        //Set the Singleton S to be this instance of particle spawner
+        S = this;
+        //Start instantiation of the particles
+        ParticleUnitys = new List<ParticleUnity>();//the list holding the particles
+        _SpawnPoints = GetPointsOnSphere(numParticles);
+        InstantiateParticleUnitys();
+    }
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 180f);
+    }
+    //a method to spawn the particles
+    public void InstantiateParticleUnitys()
+    {
+        Vector3 SpawnPosition = particleAnchor.position + (_SpawnPoints[ParticleUnitys.Count] * SpawnRadius);
+        GameObject go = Instantiate(particleUnityPrefab,SpawnPosition,Quaternion.identity);
+        ParticleUnity b = go.GetComponent<ParticleUnity>();
+        //b.transform.SetParent(particleAnchor, true);
+        b.MyParent = this.transform;
+        b.TargetOffset = (_SpawnPoints[ParticleUnitys.Count] * SpawnRadius);
+        ParticleUnitys.Add(b);
+        if (ParticleUnitys.Count < numParticles)
+        {
+            Invoke("InstantiateParticleUnitys", spawnDelay);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3[] points = GetPointsOnSphere(100);
+        foreach(Vector3 point in points)
+        {
+           // Gizmos.DrawSphere(point * SpawnRadius, 1f);
+        }
+    }
+    Vector3[] GetPointsOnSphere(int nPoints)
+    {
+        float fPoints = (float)nPoints;
+
+        Vector3[] points = new Vector3[nPoints];
+
+        float inc = Mathf.PI * (3 - Mathf.Sqrt(5));
+        float off = 2 / fPoints;
+
+        for (int k = 0; k < nPoints; k++)
+        {
+            float y = k * off - 1 + (off / 2);
+            float r = Mathf.Sqrt(1 - y * y);
+            float phi = k * inc;
+
+            points[k] = new Vector3(Mathf.Cos(phi) * r, y, Mathf.Sin(phi) * r);
+        }
+
+        return points;
+    }
+}
